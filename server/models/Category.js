82579b8e2e45799243289cf48fbfeb64b0dccc73
Joshuarupasinghe <joshuajrupasinghe@gmail.com>
2025-04-27 21:28:25 +0530

    Refactor CategorySection to remove unused state and improve code clarity; add Service and Subcategory models for better data structure

--- server/models/Category.js
+++ server/models/Category.js
@@ -1,25 +1,20 @@
 const mongoose = require('mongoose');
-const { transformAuthInfo } = require('passport');
 
 const CategorySchema = new mongoose.Schema({
-    name: {
-        type: String,
-        required: true,
-        unique: true,
-        trim: true
-    },
-
-    subcategories: [{
-        type: String,
-        trim: true
-    }],
-
-    image: {
-        type: String,
-        default: ''
-    }
-}, {
-    timestamps: true
-});
+  name: {
+    type: String,
+    required: true,
+    unique: true,
+    trim: true
+  },
+  image: {
+    type: String,
+    default: ''
+  },
+  subcategories: [{
+    type: mongoose.Schema.Types.ObjectId,
+    ref: 'Subcategory'
+  }]
+}, { timestamps: true });
 
 module.exports = mongoose.model('Category', CategorySchema);
